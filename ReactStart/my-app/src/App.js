6f6a65c0562291bafe4c93aff3804d819f11c052
Shiou <ai791016ai791016@gmail.com>
2018-10-21 02:16:43 +0800

    first commit

---
+++ ReactStart/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import HelloWorld from './HelloWorld'
+
+class App extends Component {
+  render() {
+    return (
+      <HelloWorld text="That's start React!" />
+    )
+  }
+}
+
+export default App
---
+++ gulp_env/readme.txt
@@ -0,0 +1,7 @@
+
+React 環境建置過程
+browserify 套件
+gulp (--save-dev)
+babel (--save-dev) (將jsx ES6 轉換的翻譯機)
+
+利用gulp 做watch的動作
